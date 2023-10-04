77fd35a12204539c1a73926e1d4ed365b482d21a
mamzil <marwane.amzil-ext@savoye.com>
2023-10-04 12:03:04 +0200

    Adding decryption possibility, modifying all the controllers functions for those who need to use decryption

---
+++ Back/mongodb_js/models/userModel.js
@@ -0,0 +1,98 @@
+// Imports
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+require('dotenv').config();
+
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+  },
+  surname: {
+    type: String,
+    required: [true, 'Surname is required'],
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /.+@.+\..+/.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
+    },
+  },
+  role: {
+    type: String,
+    required: [true, 'No Role or Wrong role'],
+    enum: ['admin', 'user'],
+  },
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  last_login: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  }
+});
+
+const encryptField = (field, secretKey) => {
+  const iv = crypto.randomBytes(16);  // Initialization vector of 16 bytes (128 bits)
+  const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
+  const encryptedData = cipher.update(field, 'utf8', 'hex') + cipher.final('hex');
+  return iv.toString('hex') + encryptedData;  // Prepend IV to encrypted data
+};
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  // Fetch the secret key from the environment variable
+  const secretKey = Buffer.from(process.env.SECRET_KEY, 'hex');  
+
+  user.name = encryptField(user.name, secretKey);
+  user.surname = encryptField(user.surname, secretKey);
+  user.username = encryptField(user.username, secretKey);
+  user.email = encryptField(user.email, secretKey);
+
+  next();
+});
+
+// Hash the password
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
+});
+
+/*
+ * Key Points:
+ * - The `pre('save')` middleware functions automatically run before a document is saved.
+ * - When creating or updating users via the exported model, the encryption and hashing processes 
+ *   are applied automatically due to the attached middleware.
+*/
+  module.exports = mongoose.model('User', UserSchema);
