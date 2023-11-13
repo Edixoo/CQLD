b27587e4e4c0426665b6077a1090fe96f4834649
Antoinebac435 <antoinebacquet62@gmail.com>
2023-11-13 08:36:33 +0100

    Update PageCategoriesUnit.vue

--- front/src/pages/PageCategoriesUnit.vue
+++ front/src/pages/PageCategoriesUnit.vue
@@ -1,9 +1,58 @@
 <template>
   <q-page>
-    <h1>PageCategoriesUnit</h1>
+    <q-card style="max-width: 600px" class="q-mx-auto q-pa-md q-py-md">
+      <q-card-section>
+        <div class="q-gutter-md">
+          <h2 class="text-h4" style="text-align: center">
+            Mot 1 VS  Mot 2
+          </h2>
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section>
+        <div>
+          <p>Description de la différence entre Mot 1 et Mot 2</p>
+        </div>
+      </q-card-section>
+      <q-separator />
+
+      <q-card-section>
+        <div class="q-mt-md">
+                <label for="catégories" class="label-large">Catégories</label>
+                <div>
+                  <q-btn-dropdown
+                    flat
+                    class="q-mr-md btn-dropdown-pageCreate"
+                    label="zzzzzzzzzz"
+                  >
+                    <q-list>
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Paul</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Chiens</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable v-close-popup @click="onItemClick">
+                        <q-item-section>
+                          <q-item-label>Marine</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-btn-dropdown>
+                </div>
+            </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
-<script setup>
-
+<script>
 </script>
