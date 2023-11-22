a3380399214292e18f8ed7b2ba2223a2f9e54fa5
woorz <marinelangrez@outlook.fr>
2023-11-22 22:22:33 +0100

    update ConnexionButton dynamic colors

--- front/src/pages/PageAdmin.vue
+++ front/src/pages/PageAdmin.vue
@@ -1,88 +1,36 @@
 <template>
-  <q-page>
-    <div class="q-pa-xl">
-      <q-markup-table class="table">
-        <thead class="header">
-          <tr>
-            <th class="text-left">Mot1</th>
-            <th class="text-right">Mot2</th>
-            <th class="text-right">Catégorie</th>
-            <th class="text-right">Description</th>
-            <th class="text-right">Inspecter</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(mot, index) in mots" :key="index">
-            <td class="text-left">{{ mot.mot1 }}</td>
-            <td class="text-right">{{ mot.mot2 }}</td>
-            <td class="text-right">{{ mot.categorie }}</td>
-            <td class="text-right">{{ mot.description }}</td>
-            <td class="text-right" >
-              <q-btn class="button" @click="inspectWord(index)" label="Inspecter"/>
-            </td>
-          </tr>
-        </tbody>
-      </q-markup-table>
-    </div>
-
-    <q-dialog v-model="dialog">
-      <q-card style="max-width: 600px" class="q-mx-auto q-pa-md q-py-md">
-        <q-card-section>
-          <div class="q-gutter-md">
-            <h2 class="text-h4" style="text-align: center">
-              Mot 1 VS  Mot 2
-            </h2>
-          </div>
-        </q-card-section>
-  
-        <q-separator />
-  
-        <q-card-section>
-          <div>
-            <p> Description de la différence entre Mot 1 et Mot 2 </p>
-          </div>
-        </q-card-section>
-        <q-separator />
-  
-        <q-card-section>
-          <div class="q-mt-md">
-                  <label for="catégories" class="label-large">Catégories</label>
-                  <div>
-                    <q-btn-dropdown
-                      flat
-                      class="q-mr-md btn-dropdown-pageCreate"
-                      label="zzzzzzzzzz"
-                    >
-                      <q-list>
-                        <q-item clickable v-close-popup @click="onItemClick">
-                          <q-item-section>
-                            <q-item-label>Paul</q-item-label>
-                          </q-item-section>
-                        </q-item>
-  
-                        <q-item clickable v-close-popup @click="onItemClick">
-                          <q-item-section>
-                            <q-item-label>Chiens</q-item-label>
-                          </q-item-section>
-                        </q-item>
-  
-                        <q-item clickable v-close-popup @click="onItemClick">
-                          <q-item-section>
-                            <q-item-label>Marine</q-item-label>
-                          </q-item-section>
-                        </q-item>
-                      </q-list>
-                    </q-btn-dropdown>
-                  </div>
-              </div>
-        </q-card-section>
-        <q-card-actions align="right">
-          <q-btn color="negative" label="Annuler" />
-          <q-btn color="positive" label="Valider" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-  </q-page>
+  <div class="q-pa-md">
+    <q-markup-table>
+      <thead style="background-color: #54546c; color: white">
+        <tr>
+          <th class="text-left">Mot1</th>
+          <th class="text-right">Mot2</th>
+          <th class="text-right">Catégorie</th>
+          <th class="text-right">Description</th>
+          <th class="text-right">Inspecter</th>
+        </tr>
+      </thead>
+      <tbody template v-for="(mot, index) in mots" :key="index">
+        <tr>
+          <td class="text-left">{{ mot.mot1 }}</td>
+          <td class="text-right">{{ mot.mot2 }}</td>
+          <td class="text-right">{{ mot.categorie }}</td>
+          <td class="text-right">{{ mot.description }}</td>
+          <td class="text-right">
+            <q-btn
+              @click="inspectWord(index)"
+              size="12px"
+              flat
+              dense
+              round
+              icon="search"
+              >Inspecter</q-btn
+            >
+          </td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </div>
 </template>
 
 <script setup>
