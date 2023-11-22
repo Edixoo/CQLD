56fb98e891c33e8c7b0f81a33245e66e53b21d4e
woorz <marinelangrez@outlook.fr>
2023-11-22 23:11:11 +0100

    update PersonnalData

--- front/src/pages/PagePersonnalData.vue
+++ front/src/pages/PagePersonnalData.vue
@@ -1,85 +1,91 @@
 <template>
   <q-page>
     <div class="q-pa-md" style="padding: 0%; margin: 0%">
-      <p class="first-paragraph">
-        Bienvenue sur la page de données personnelles de CQLD !
-      </p>
-      <p>
-        Chez CQLD, nous accordons une grande importance à la protection de la
-        vie privée de nos utilisateurs. Cette politique de confidentialité
-        explique quelles données personnelles nous collectons, comment nous les
-        utilisons, et les droits dont vous disposez concernant vos informations.
-      </p>
+      <div class="pagePersonnalData">
+        <div class="width">
+          <h4 class="h4">Données personnelles</h4>
+        </div>
 
-      <section>
-        <h3>Collecte de Données Personnelles</h3>
-        <p>
-          Nous collectons des données personnelles lorsque vous utilisez nos
-          services. Ces informations peuvent inclure votre nom, votre adresse
-          e-mail, votre adresse IP, etc. Nous collectons ces données dans le but
-          de vous fournir nos services, d'améliorer votre expérience
-          utilisateur, et de personnaliser le contenu que vous voyez.
-        </p>
-      </section>
+        <q-separator />
 
-      <section>
-        <h3>Consentement</h3>
-        <p>
-          En utilisant nos services, vous consentez à la collecte et au
-          traitement de vos données personnelles telles que décrites dans cette
-          politique de confidentialité. Vous pouvez retirer votre consentement à
-          tout moment en nous contactant à l'adresse fournie en bas de cette
-          page.
+        <p class="width first-p">
+          Chez CQLD, nous accordons une grande importance à la protection de la
+          vie privée de nos utilisateurs. Cette politique de confidentialité
+          explique quelles données personnelles nous collectons, comment nous
+          les utilisons, et les droits dont vous disposez concernant vos
+          informations.
         </p>
-      </section>
 
-      <section>
-        <h3>Droits des Utilisateurs</h3>
-        <p>
-          Vous avez le droit d'accéder à vos données personnelles, de les
-          rectifier, de les supprimer, de vous opposer à leur traitement, et
-          plus encore. Pour exercer vos droits, veuillez nous contacter à
-          [adresse e-mail].
-        </p>
-      </section>
+        <section class="width">
+          <h6 class="h6">Collecte de Données Personnelles</h6>
+          <p>
+            Nous collectons des données personnelles lorsque vous utilisez nos
+            services. Ces informations peuvent inclure votre nom, votre adresse
+            e-mail, votre adresse IP, etc. Nous collectons ces données dans le
+            but de vous fournir nos services, d'améliorer votre expérience
+            utilisateur, et de personnaliser le contenu que vous voyez.
+          </p>
+        </section>
 
-      <section>
-        <h3>Sécurité des Données</h3>
-        <p>
-          La sécurité de vos données personnelles est une priorité pour nous.
-          Nous avons mis en place des mesures de sécurité robustes pour protéger
-          vos informations contre tout accès non autorisé, perte ou
-          modification.
-        </p>
-      </section>
+        <section class="width">
+          <h6 class="h6">Consentement</h6>
+          <p>
+            En utilisant nos services, vous consentez à la collecte et au
+            traitement de vos données personnelles telles que décrites dans
+            cette politique de confidentialité. Vous pouvez retirer votre
+            consentement à tout moment en nous contactant à l'adresse fournie en
+            bas de cette page.
+          </p>
+        </section>
 
-      <section>
-        <h3>Partage de Données</h3>
-        <p>
-          Nous ne partageons vos données personnelles avec des tiers que dans le
-          cadre de la prestation de nos services. Nous ne vendons ni ne louons
-          vos informations à des tiers à des fins de marketing.
-        </p>
-      </section>
+        <section class="width">
+          <h6 class="h6">Droits des Utilisateurs</h6>
+          <p>
+            Vous avez le droit d'accéder à vos données personnelles, de les
+            rectifier, de les supprimer, de vous opposer à leur traitement, et
+            plus encore. Pour exercer vos droits, veuillez nous contacter à
+            cqld-iut@gmail.com
+          </p>
+        </section>
 
-      <section>
-        <h3>Contact</h3>
-        <p>
-          Si vous avez des questions concernant notre politique de
-          confidentialité ou si vous souhaitez exercer vos droits en matière de
-          données personnelles, veuillez nous contacter à [adresse postale] ou
-          par e-mail à [adresse e-mail].
-        </p>
-      </section>
+        <section class="width">
+          <h6 class="h6">Sécurité des Données</h6>
+          <p>
+            La sécurité de vos données personnelles est une priorité pour nous.
+            Nous avons mis en place des mesures de sécurité robustes pour
+            protéger vos informations contre tout accès non autorisé, perte ou
+            modification.
+          </p>
+        </section>
 
-      <section>
-        <h3>Mises à jour</h3>
-        <p class="last-paragraph">
-          Cette politique de confidentialité peut être mise à jour
-          périodiquement. Toute modification importante sera communiquée de
-          manière appropriée.
-        </p>
-      </section>
+        <section class="width">
+          <h6 class="h6">Partage de Données</h6>
+          <p>
+            Nous ne partageons vos données personnelles avec des tiers que dans
+            le cadre de la prestation de nos services. Nous ne vendons ni ne
+            louons vos informations à des tiers à des fins de marketing.
+          </p>
+        </section>
+
+        <section class="width">
+          <h6 class="h6">Contact</h6>
+          <p>
+            Si vous avez des questions concernant notre politique de
+            confidentialité ou si vous souhaitez exercer vos droits en matière
+            de données personnelles, veuillez nous contacter par e-mail à
+            cqld-iut@gmail.com
+          </p>
+        </section>
+
+        <section class="width">
+          <h6 class="h6">Mises à jour</h6>
+          <p class="last-paragraph">
+            Cette politique de confidentialité peut être mise à jour
+            périodiquement. Toute modification importante sera communiquée de
+            manière appropriée.
+          </p>
+        </section>
+      </div>
     </div>
   </q-page>
 </template>
@@ -97,16 +103,24 @@ export default {
 </script>
 
 <style lang="scss">
-h3 {
+.h6 {
+  margin-top: 50px;
+  margin-bottom: 10px;
   font-weight: 500;
-  text-align: center;
-  font-size: 30px;
   color: $third;
+  width: 80%;
+}
+
+.h4 {
+  font-weight: 500;
+  color: $third;
+  width: 80%;
 }
 
 p {
   font-weight: 400;
-  font-size: 18px;
+  line-height: 28px;
+  font-size: 15px;
   letter-spacing: 0.5px;
   text-align: justify;
   color: $third;
@@ -114,11 +128,16 @@ p {
   width: 80%;
 }
 
-.first-paragraph {
-  margin-top: 60px;
+.width {
+  display: grid;
+  justify-items: center;
 }
 
-.last-paragraph {
-  margin-bottom: 50px;
+.first-p {
+  margin-top: 40px;
+}
+
+.width:last-child {
+  margin-bottom: 80px;
 }
 </style>
