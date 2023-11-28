a01061b3e729f3ab2ba1ca669a1d4feaa6795d53
Paul Pruvost <paul1pruvost@gmail.com>
2023-11-28 10:24:06 +0100

    Add CategorieCreate in adminpage

--- front/src/pages/PageAdmin.vue
+++ front/src/pages/PageAdmin.vue
@@ -1,6 +1,13 @@
 <template>
+  <q-page>
   <div class="q-pa-md">
-    <q-markup-table>
+    <app-title title="Administration des liens" class="q-mb-xl"/>
+    <q-btn
+      color="primary"
+      label="Créer une catégorie"
+      @click="newThemeAffichage = !newThemeAffichage"
+      />
+    <q-markup-table class="q-mt-xl">
       <thead style="background-color: #54546c; color: white">
         <tr>
           <th class="text-left">Mot1</th>
@@ -56,21 +63,70 @@
       </tbody>
     </q-markup-table>
   </div>
+
+
+  <q-dialog v-model="newThemeAffichage">
+    <q-card class="q-pa-md q-card-modif-pwd">
+      <q-card-section>
+        <div class="text-h5 title-card">Création d'une catégorie</div>
+        <div class="q-mt-md">
+          Veuillez entrer ci-dessous le nom de la catégorie
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-input
+          filled
+          v-model="theme_name"
+          label="Nom de la catégorie"
+          placeholder="Entrer le nom de la catégorie"
+        />
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn
+          label="Annuler"
+          color="negative"
+          @click="resetTheme()"
+        />
+        <q-btn label="Envoyer" color="positive" @click="createTheme()" />
+      </q-card-actions>
+    </q-card>
+    </q-dialog>
+  </q-page>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import ConnexionServices from "../services/ConnexionServices";
 import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
+import ThemeServices from "src/services/ThemeServices";
+import AppTitle from "src/components/ui/AppTitle.vue";
 
 const $q = useQuasar();
 const $router = useRouter();
 const mots = ref([]);
+const newThemeAffichage = ref(false);
+const theme_name = ref("");
+
+const getThemeName= (async (themeId) => {
+  const theme = await ThemeServices.getThemeById(themeId)
+  return theme.theme_name;
+});
+
+watch(newThemeAffichage, (newValue) => {
+  if (!newValue) {
+    resetTheme();
+  }
+});
 
 onMounted(async () => {
   try {
     mots.value = await ConnexionServices.getConnectionsByApproved();
+    for (let i = 0; i < mots.value.length; i++) {
+      mots.value[i].theme = await getThemeName(mots.value[i].theme);
+    }
   } catch (error) {
     console.error("Erreur lors de la récupération des connexions non approuvées:", error);
   }
@@ -89,7 +145,7 @@ const approuverMot = async (connexion) => {
         message: "Le mot a bien été approuvé",
         icon: "done",
       });
-      $router.push('/admin');
+      window.location.reload();
     } else {
       console.error("Erreur lors de l'approbation du mot : ID non défini ou introuvable.");
     }
@@ -107,7 +163,7 @@ const refuserMot = async (connexion) => {
         message: "Le mot a été refusé et supprimé",
         icon: "close",
       });
-      $router.push('/admin');
+      window.location.reload();
     } else {
       console.error("Erreur lors du refus et de la suppression du mot : ID non défini ou introuvable.");
     }
@@ -115,6 +171,29 @@ const refuserMot = async (connexion) => {
     console.error("Erreur lors du refus et de la suppression du mot :", error);
   }
 };
+
+const resetTheme= (() => {
+  theme_name.value = "";
+});
+
+const createTheme= (() => {
+  if (theme_name.value) {
+    ThemeServices.createTheme({ theme_name: theme_name.value });
+    $q.notify({
+      color: "positive",
+      message: "La catégorie a bien été créée",
+      icon: "done",
+    });
+    newThemeAffichage.value = false;
+    window.location.reload();
+  } else {
+    $q.notify({
+      color: "negative",
+      message: "Veuillez entrer un nom de catégorie",
+      icon: "close",
+    });
+  }
+});
 </script>
 
 <style>
